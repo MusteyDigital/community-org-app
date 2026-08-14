7c9a89cddb8fcc14ac8ac15be1e3557e631a699d
Mustapha Adamu <mustaphaadamu6773@gmail.com>
2026-08-14 06:02:00 -0700

    Show notification bell on mobile nav next to hamburger menu

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -45,7 +45,8 @@
                     </x-slot>
                 </x-dropdown>
             </div>
-            <div class="-me-2 flex items-center sm:hidden">
+            <div class="-me-2 flex items-center gap-1 sm:hidden">
+                @include('layouts.notification-bell')
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-sand-100 hover:bg-teal-800 focus:outline-none transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -83,6 +84,7 @@
         </div>
     </div>
 </nav>
+
 
 
 
